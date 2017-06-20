ddce76cff0ba470ad248082cdf31e4b044d1626d
borisvorob <borisss777@gmail.com>
2017-06-20 10:42:13 +0300

    Mobile

--- built/assets/scripts/main.js
+++ built/assets/scripts/main.js
@@ -4,7 +4,9 @@ $(document).ready(function () {
 
   $('.main-slider').slick({
     fade: true,
-    dots: true
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 10000
   });
 
   $('.list-slider').slick({
@@ -15,9 +17,10 @@ $(document).ready(function () {
 // Google map
 function initMap() {
   var deco = { lat: 47.026442, lng: 28.830943 };
+  var position = { lat: 47.026442, lng: 28.83243 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
-    center: deco,
+    center: position,
     scrollwheel: false
   });
   var marker = new google.maps.Marker({
